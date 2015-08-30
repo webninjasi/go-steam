80649647c0be34b543858238a39b4b9902dc840e
soft as HELL <softashell@cock.li>
2015-08-30 03:51:01 +0300

    Regenerate internals

--- internal/protobuf/unified/partnerapps.pb.go
+++ internal/protobuf/unified/partnerapps.pb.go
@@ -7,6 +7,7 @@ package unified
 import proto "github.com/golang/protobuf/proto"
 import math "math"
 
+
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 var _ = math.Inf
